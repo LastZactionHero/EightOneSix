699e3bd0d70ebce66113e1937820b7a70ab6191c
Zach Dicklin <zdicklin@zdicklin-laptop.(none)>
2010-11-22 22:08:18 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eightonesix::Application.config.secret_token = 'e88b6600794290bf0ed9c3887263848e19de193588a8a66ab971409c5a29eea8628d0680d0b22c06b4f8cb35b17544aec45094084f82caadaaa8b7cb8967ff9b'
